279dadacff600b654c35a6b43d8e981b37db526d
cfidrobo <cfidrobo@espe.edu.ec>
2025-10-28 10:25:35 -0500

    Pruebas de terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -16,6 +20,14 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name = var.resource_group_name
 }
+
+# sufijo aleatorio para que el FQDN sea único
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 
 resource "azurerm_container_group" "main" {
   name                = "devops-service-aci"
