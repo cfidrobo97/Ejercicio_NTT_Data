fa09140f3261f11a4a1e7cf303d81ec2c6fc4e93
cfidrobo <cfidrobo@espe.edu.ec>
2025-10-27 23:58:10 -0500

    feat(terraform): configuración de providers Azure

---
+++ terraform/versions.tf
@@ -0,0 +1,23 @@
+# Configuración de versiones de providers necesarios
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configurar el proveedor de Azure
+provider "azurerm" {
+  features {}
+}
+
+
+
