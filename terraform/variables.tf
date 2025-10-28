cd19572ed1a15388713674c9dfd97adb7476f4e1
cfidrobo <cfidrobo@espe.edu.ec>
2025-10-28 01:55:21 -0500

    feat(terraform): Ajustar las variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,32 @@
 variable "resource_group_name" {
-  description = "Nombre del RG existente"
   type        = string
   default     = "devops-service-rg"
+  description = "Nombre del RG existente"
 }
 
 variable "location" {
-  description = "Región de Azure (para crear RG si no existe desde az)"
   type        = string
   default     = "eastus"
+  description = "Región del RG (para crearlo con az si no existe)"
 }
 
 variable "app_service_name" {
-  description = "Nombre de la App Service"
   type        = string
   default     = "devops-service-app"
 }
 
 variable "app_service_plan_name" {
-  description = "Nombre del App Service Plan"
   type        = string
   default     = "devops-service-plan"
 }
 
 variable "ghcr_token" {
-  description = "Token de GHCR (read:packages)"
   type        = string
   sensitive   = true
+  description = "PAT de GHCR con read:packages"
 }
 
 variable "docker_image" {
-  description = "Imagen Docker en GHCR o ACR (ej: ghcr.io/owner/repo:latest)"
   type        = string
+  description = "Imagen Docker (p.ej.: ghcr.io/owner/repo:latest)"
 }
